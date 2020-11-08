cc179269618a411806f2364777696f5fb2a3c1d4
TranKai <quyettran057@gmail.com>
2020-11-08 12:45:21 +0700

    new updated code!

--- src/com/team/controller/UpdateCartController.java
+++ src/com/team/controller/UpdateCartController.java
@@ -1,11 +1,16 @@
 package com.team.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.team.model.Item;
+import com.team.model.Order;
 
 /**
  * Servlet implementation class UpdateCartController
@@ -28,8 +33,7 @@ public class UpdateCartController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/view/user/template/shopping-cart").forward(request, response);
 	}
 
 	/**
@@ -38,8 +42,19 @@ public class UpdateCartController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		HttpSession session = request.getSession();
+		Order order = (Order) session.getAttribute("order");
+		List<Item> listItems = order.getItems();
+		order.setSumPrice(0);
+		listItems.forEach(p -> {
+			p.setAmount(p.getProduct().getProduct_id());
+			p.setPrice(p.getProduct().getPrice());
+			order.setSumPrice(order.getSumPrice() + p.getPrice());
+		});
+		order.setItems(listItems);
+		session.setAttribute("order", order);
+		session.setAttribute("sumprice", order.getSumPrice());
+		response.sendRedirect(request.getContextPath() + "/view/user/shopping-cart");
 	}
 
 }
