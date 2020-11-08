d7fb4952c9f96a1b74fe137c33c7b2df3e425045
TranKai <quyettran057@gmail.com>
2020-11-08 12:32:27 +0700

    new updated code!

--- src/com/team/controller/DeleteProductInCartController.java
+++ src/com/team/controller/DeleteProductInCartController.java
@@ -1,6 +1,7 @@
 package com.team.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.team.dao.impl.ProductDaoImpl;
+import com.team.model.Item;
 import com.team.model.Order;
 import com.team.model.Product;
 
@@ -37,6 +39,25 @@ public class DeleteProductInCartController extends HttpServlet {
 		String id = request.getParameter("id");
 		HttpSession session = request.getSession();
 		Product product = new ProductDaoImpl().get(id);
-		Order order = (Order)session.getAttribute("order");
+		Order order = (Order) session.getAttribute("order");
+		List<Item> listItems = order.getItems();
+		/*
+		 * listItems.stream().forEach(p -> { if (p.getProduct().getProduct_id() ==
+		 * product.getProduct_id()) { order.setSumPrice(order.getSumPrice() -
+		 * p.getPrice()); listItems.remove(p);
+		 * System.out.println("Delete cart successfully!"); } });
+		 */
+		listItems.stream().filter(p -> p.getProduct().getProduct_id() == product.getProduct_id())
+				.forEach(p -> listItems.remove(p));
+
+		order.setItems(listItems);
+		session.setAttribute("order", order);
+		response.sendRedirect(request.getContextPath() + "/view/user/shopping-cart");
+
+		if (order.getSumPrice() == 0) {
+			session.setAttribute("sumprice", "0");
+		} else {
+			session.setAttribute("sumprice", order.getSumPrice());
+		}
 	}
 }
