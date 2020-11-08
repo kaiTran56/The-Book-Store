1547688c7127ca055cf7f285813de00668264c23
TranKai <quyettran057@gmail.com>
2020-11-08 09:56:18 +0700

    new updated code!

---
+++ src/com/team/service/impl/TransactionServiceImpl.java
@@ -0,0 +1,40 @@
+package com.team.service.impl;
+
+import java.util.List;
+
+import com.team.model.Transactions;
+import com.team.service.TransactionService;
+
+public class TransactionServiceImpl implements TransactionService {
+
+	@Override
+	public void insert(Transactions transaction) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void edit(Transactions transaction) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Transactions> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Transactions get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
