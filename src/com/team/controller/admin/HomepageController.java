63ee89bbffaf5fe68a426999970d0d94b9349104
TranKai <quyettran057@gmail.com>
2020-11-13 23:07:12 +0700

    new updated code!

--- src/com/team/controller/admin/HomepageController.java
+++ src/com/team/controller/admin/HomepageController.java
@@ -13,19 +13,21 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HomepageController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HomepageController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public HomepageController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.getRequestDispatcher("/view/admin/index.jsp").forward(request, response);
 	}
 
