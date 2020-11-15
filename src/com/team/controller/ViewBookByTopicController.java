9bccf8b3facaf9f1ccc07484601ce924cb16d35b
TranKai <quyettran057@gmail.com>
2020-11-15 22:42:07 +0700

    new updated code!

--- src/com/team/controller/ViewBookByTopicController.java
+++ src/com/team/controller/ViewBookByTopicController.java
@@ -1,11 +1,16 @@
 package com.team.controller;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.team.dao.impl.ProductDaoImpl;
+import com.team.model.Product;
 
 /**
  * Servlet implementation class ViewBookByTopic
@@ -27,8 +32,10 @@ public class ViewBookByTopicController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//String author = request.getParameter("check-author");
+		String author = "Nguyen Nhat Anh";
+		List<Product> listTopic = new ProductDaoImpl().getAll().stream().filter(p -> p.getAuthor().equalsIgnoreCase(author))
+				.collect(Collectors.toList());
 	}
 
 }
